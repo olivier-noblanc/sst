995fb465ca3602d140f0524092ef22ba736a7fcf
NOBLANC <olivier.noblanc@dreets.gouv.fr>
2026-08-05 17:20:17 +0200

    fix(session): set cookie_path to '/' for consistent cookie sending

--- src/Services/SessionService.php
+++ src/Services/SessionService.php
@@ -38,6 +38,7 @@ class SessionService
             ini_set('session.use_only_cookies', '1');
             ini_set('session.cookie_httponly', '1');
             ini_set('session.cookie_samesite', 'Lax');
+            ini_set('session.cookie_path', '/');
             if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') {
                 ini_set('session.cookie_secure', '1');
             }
